c5717b0474d5cccc4e639a90fcca6401a10e5e4d
Joel Rosario <joelenrosario@gmail.com>
2023-07-16 14:26:45 -0400

    link para menu de cruds del paciente

--- menu-mant.php
+++ menu-mant.php
@@ -1,46 +1,46 @@
-
-
 <html>
+
 <head>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 
 </head>
+
 <body>
-    <h2 style="padding: 5%;text-align: center;text-transform: uppercase;">Menú Principal Sistema de Pediatría</h2>
- 
-    <a href="menu-Mant.html">
-<div class="card">
-  <div class="card-title">Mantenimientos</div>
-   
-  <img src="IMAGENES/Mantenimientos.png" class="card-icon" alt="Mantenimientos">
-         
-  <div class="card-description">Esta opción te permite realizar operaciones de mantenimiento sobre las tablas de la base de datos, como crear, modificar, eliminar o consultar registros. También puedes realizar copias de seguridad y restaurar la información en caso de pérdida o daño.</div>
-</div>
-         </a>
+  <h2 style="padding: 5%;text-align: center;text-transform: uppercase;">Menú de Mantenimientos</h2>
 
-<div class="card">
-  <div class="card-title">Procesos</div>
-  <img src="IMAGENES/procesos.png" class="card-icon" alt="Procesos">
-  <div class="card-description">Esta opción te permite ejecutar procesos que involucran varias tablas de la base de datos, como calcular el salario de los empleados, generar facturas, actualizar el inventario, etc. Puedes ver el resultado de los procesos en pantalla o en archivos.</div>
-</div>
+  <a href="menu-Mant.html">
+    <div class="card">
+      <div class="card-title">Pacientes</div>
 
-<div class="card">
-  <div class="card-title">Consultas</div>
-  <img src="IMAGENES/consultas.png" class="card-icon" alt="Consultas">
-  <div class="card-description">Esta opción te permite realizar consultas personalizadas sobre la base de datos, usando criterios de selección, ordenación, agrupación y filtrado. Puedes ver el resultado de las consultas en pantalla o en archivos.</div>
-</div>
+      <img src="IMAGENES/Mantenimientos.png" class="card-icon" alt="Mantenimientos">
 
-<div class="card">
-  <div class="card-title">Reportes</div>
-  <img src="IMAGENES/reportes.png" class="card-icon" alt="Reportes">
-  <div class="card-description">Esta opción te permite generar reportes gráficos o estadísticos sobre la base de datos, usando diferentes tipos de gráficos, como barras, líneas, tortas, etc. Puedes ver el resultado de los reportes en pantalla o en archivos.</div>
-</div>
-    <div style='width: 100%;float: left;'>
-      <form action="index.php" method="POST">
-        <input type="submit" value="Regresar al login">
+      <div class="card-description">En este apartado vamos a encontrar todo lo referente a los Pacientes, como registrar el paciente, vicular el registro de padres/madres o tutores de cada niño paciente, historia clínica, antecedentes médicos, vacunas, nutrición</div>
+    </div>
+  </a>
+
+  <div class="card">
+    <div class="card-title">Procesos</div>
+    <img src="IMAGENES/procesos.png" class="card-icon" alt="Procesos">
+    <div class="card-description">Esta opción te permite ejecutar procesos que involucran varias tablas de la base de datos, como calcular el salario de los empleados, generar facturas, actualizar el inventario, etc. Puedes ver el resultado de los procesos en pantalla o en archivos.</div>
+  </div>
+
+  <div class="card">
+    <div class="card-title">Consultas</div>
+    <img src="IMAGENES/consultas.png" class="card-icon" alt="Consultas">
+    <div class="card-description">Esta opción te permite realizar consultas personalizadas sobre la base de datos, usando criterios de selección, ordenación, agrupación y filtrado. Puedes ver el resultado de las consultas en pantalla o en archivos.</div>
+  </div>
+
+  <div class="card">
+    <div class="card-title">Reportes</div>
+    <img src="IMAGENES/reportes.png" class="card-icon" alt="Reportes">
+    <div class="card-description">Esta opción te permite generar reportes gráficos o estadísticos sobre la base de datos, usando diferentes tipos de gráficos, como barras, líneas, tortas, etc. Puedes ver el resultado de los reportes en pantalla o en archivos.</div>
+  </div>
+  <div style='width: 100%;float: left;'>
+    <form action="index.php" method="POST">
+      <input type="submit" value="Regresar al login">
     </form>
-</div>
+  </div>
 
 </body>
-</html>
 
+</html>
