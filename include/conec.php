c439bfd1a03e2bc92cf3ead1dcf7cfda12c80f35
Joel Rosario <joelenrosario@gmail.com>
2023-07-16 11:13:41 -0400

    archivo conn para conexion con base de datos de forma general en formato php

---
+++ include/conec.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "pediatra_sis";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+// Resto del código...
+
+// Cerrar la conexión
+$conn->close();
+
+?>
