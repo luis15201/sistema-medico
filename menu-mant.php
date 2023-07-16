1aa3bae953039a8ac45f69d3dba5681835a76d63
Joel Rosario <joelenrosario@gmail.com>
2023-07-16 18:10:49 -0400

    modificacion de algunos elementos esteticos

--- menu-mant.php
+++ menu-mant.php
@@ -2,43 +2,46 @@
 
 <head>
   <link rel="stylesheet" type="text/css" href="css/style.css">
-
+  <link href="https://fonts.googleapis.com/css?family=Anton:regular" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Bitter:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic" rel="stylesheet" />
 </head>
 
-<body>
-  <h2 style="padding: 5%;text-align: center;text-transform: uppercase;">Menú de Mantenimientos</h2>
-
+<body style="background: linear-gradient(to right, #7789, #fff6);">
+  <div style="width:100%; padding: 6px; width: 100%; display: flex; flex-direction: column; align-items: center;">
+    <h2 style="padding: 1%;text-align: center;text-transform: uppercase;font-family: bitter; color:black; padding: 15px; width: 100%; display: flex; flex-direction: column; align-items: center;">Menú de Mantenimientos</h2>
+    <img src="IMAGENES\app90.png" class="" alt="crud">
+  </div>
   <a href="menu-Mant.html">
-    <div class="card">
-      <div class="card-title">Pacientes</div>
+    <div class="card" style="background: linear-gradient(to right, #E8A9F7,#e4e5dc ); ">
+      <div class="card-title" style="font-family: Anton; color:black;">Pacientes</div>
       <img src="IMAGENES\pacientes.png" class="card-icon" alt="Mantenimientos">
       <div class="card-description">En este apartado podemos encontrar todo lo referente a los Pacientes, como registrar el paciente, vincular el registro de padres/madres o tutores de cada niño paciente, historia clínica, antecedentes médicos, vacunas, nutrición</div>
     </div>
   </a>
 
   <div class="card">
-    <div class="card-title">Procesos</div>
-    <img src="IMAGENES/procesos.png" class="card-icon" alt="Procesos">
-    <div class="card-description">Esta opción te permite ejecutar procesos que involucran varias tablas de la base de datos, como calcular el salario de los empleados, generar facturas, actualizar el inventario, etc. Puedes ver el resultado de los procesos en pantalla o en archivos.</div>
+    <div class="card-title">Subproceso #1</div>
+    <img src="IMAGENES/none.png" class="card-icon" alt="Procesos">
+    <div class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget ullamcorper sapien. Integer a nisi dapibus, dignissim leo sed, porta justo. Fusce ut volutpat est, ac lacinia est. In hac habitasse platea dictumst. Nunc non ligula non mi placerat tristique. Sed id elit non elit aliquet fringilla. Vivamus in dui vitae metus semper eleifend. Quisque ullamcorper ligula vel neque vulputate eleifend.</div>
   </div>
 
   <div class="card">
-    <div class="card-title">Consultas</div>
-    <img src="IMAGENES/consultas.png" class="card-icon" alt="Consultas">
-    <div class="card-description">Esta opción te permite realizar consultas personalizadas sobre la base de datos, usando criterios de selección, ordenación, agrupación y filtrado. Puedes ver el resultado de las consultas en pantalla o en archivos.</div>
+    <div class="card-title">Subproceso #2</div>
+    <img src="IMAGENES/none.png" class="card-icon" alt="Procesos">
+    <div class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget ullamcorper sapien. Integer a nisi dapibus, dignissim leo sed, porta justo. Fusce ut volutpat est, ac lacinia est. In hac habitasse platea dictumst. Nunc non ligula non mi placerat tristique. Sed id elit non elit aliquet fringilla. Vivamus in dui vitae metus semper eleifend. Quisque ullamcorper ligula vel neque vulputate eleifend.</div>
   </div>
 
   <div class="card">
-    <div class="card-title">Reportes</div>
-    <img src="IMAGENES/reportes.png" class="card-icon" alt="Reportes">
-    <div class="card-description">Esta opción te permite generar reportes gráficos o estadísticos sobre la base de datos, usando diferentes tipos de gráficos, como barras, líneas, tortas, etc. Puedes ver el resultado de los reportes en pantalla o en archivos.</div>
+    <div class="card-title">Subproceso #3</div>
+    <img src="IMAGENES/none.png" class="card-icon" alt="Procesos">
+    <div class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget ullamcorper sapien. Integer a nisi dapibus, dignissim leo sed, porta justo. Fusce ut volutpat est, ac lacinia est. In hac habitasse platea dictumst. Nunc non ligula non mi placerat tristique. Sed id elit non elit aliquet fringilla. Vivamus in dui vitae metus semper eleifend. Quisque ullamcorper ligula vel neque vulputate eleifend.</div>
   </div>
 
 
 
-  <div class="botones-container" >
-    <a href="" class="claseboton" >Login</a>
-    <a href="" class="claseboton" >Menú Principal</a>
+  <div class="botones-container">
+    <a href="index.php" class="claseboton">Login</a>
+    <a href="menu.php" class="claseboton">Menú Principal</a>
   </div>
 
 </body>
