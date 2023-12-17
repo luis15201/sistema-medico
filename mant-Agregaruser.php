e8d98d88877efaf4158f95f3fe8361a0f0fd4063
Joel Rosario <joelenrosario@gmail.com>
2023-12-17 00:07:12 -0400

    adaptaciones para menu lateral

--- mant-Agregaruser.php
+++ mant-Agregaruser.php
@@ -2,17 +2,41 @@
 include("include/conec.php");
 
 ?>
+<?php
+
+$filasmax = 5;
+
+if (isset($_GET['pag'])) {
+    $pagina = $_GET['pag'];
+} else {
+    $pagina = 1;
+}
+
+if (isset($_POST['btnbuscar'])) {
+    $buscar = $_POST['txtbuscar'];
+
+    $sqlusu = mysqli_query($conn, "SELECT * FROM usuario where id_usuario = '" . $buscar . "'");
+} else {
+    $sqlusu = mysqli_query($conn, "SELECT * FROM usuario ORDER BY id_usuario ASC LIMIT " . (($pagina - 1) * $filasmax)  . "," . $filasmax);
+}
+
+$resultadoMaximo = mysqli_query($conn, "SELECT count(*) as num_usuarios FROM usuario");
+
+$maxusutabla = mysqli_fetch_assoc($resultadoMaximo)['num_usuarios'];
+
+?>
 <!--Busca por VaidrollTeam para más proyectos. -->
 <html>
 
 <head>
     <title>Usuarios</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="icon" href="logo.png">
-    <link rel="stylesheet" type="text/css" href="css/buttons.dataTables.min.css">
+    <!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+    <link rel="stylesheet" type="text/css" href="css/estilo-paciente.css">
+    <!--<link rel="icon" href="logo.png">-->
+    <!--link rel="stylesheet" type="text/css" href="css/buttons.dataTables.min.css"-->
     <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.min.css">
-
+    <!--link rel="stylesheet" type="text/css" href="css/datatables.css"-->
 
     <!--TODOS LOS JAVASCRIPTS DE DATATABLES-->
     <script src="js/buttons.html5.min.js"></script>
@@ -27,8 +51,7 @@ include("include/conec.php");
     <!-- Incluir CSS de DataTables cdn online -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
     <!-- Incluir CSS de DataTables Buttons cdn online-->
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
-
+    <!--link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css"-->
     <!-- Incluir jQuery y los archivos JS de DataTables y los botones desde CDN -->
     <!-- Incluir jQuery -->
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
@@ -42,49 +65,189 @@ include("include/conec.php");
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
     <!-- Incluir Botones HTML5 -->
-    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+    <!--script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script-->
+
+    <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.js"></script>
+    <link href="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.colVis.min.js"></script>
+    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.colVis.min.js"></script>
     <!-- Incluir Botones de impresión -->
     <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+
 
     <style>
-        .claseboton {
+        .caja {
+            border: 3px solid #ddd;
+            padding: 10px;
+            box-shadow: 0 0 0.5vw rgba(0, 0, 0, 0.1);
+            margin: 10px;
+            border-radius: 5px;
+        }
+
+        .cajalegend {
+            border: 0px solid rgba(102, 153, 144, 0.0);
+            font-weight: bolder;
+            font-size: 16px;
+            color: white;
+            margin: 0;
+            padding: 0;
+            background-color: transparent;
+            border-radius: 2px;
+            margin-top: -20px;
+            text-shadow: 2px 1px 2px #000000;
+
+
+        }
+
+        .container {
+            display: grid;
+            grid-template-columns: 80%;
+            grid-template-rows: repeat(3, 1fr);
+            grid-gap: 6px 10px;
+            margin-left: 10%;
+            margin-right: 10%;
+
+        }
+
+        label {
+            font-size: 14px;
+            color: #444;
+            margin: 8px;
+            font-weight: bold;
+        }
+
+        button,
+        input,
+        optgroup,
+        select,
+        textarea {
+            margin: 0;
+
+            font-size: 12px;
+            line-height: 14px;
+            margin: 10px;
+            padding-top: 5px;
+            padding-bottom: 5px;
+        }
+
+        input[type="text"],
+        input[type="date"],
+        select {
+
+            width: 150px;
+            height: 40px;
+            color: #444;
+            margin-bottom: 6%;
+            border: none;
+            border-bottom: 0.1vw solid #444;
+            outline: none;
+            border-radius: 10px;
+
+        }
+
+        button {
             border: none;
             outline: none;
-            background: linear-gradient(to right, #4a90e2, #63b8ff);
-            border-radius: 7px;
-            width: auto;
-            text-decoration: none;
-            height: 40px;
             color: #fff;
-            font-size: 16px;
-            padding: 7px;
-
-        }
-
-        .claseboton:hover {
-            background: linear-gradient(to right, #63b8ff, #4a90e2);
-        }
-
-        .botones-container {
-            margin: 2px;
-            padding: 2px;
-            box-sizing: unset;
-            width: 100%;
-            float: left;
-            text-align: center;
-            /*justify-content: center;*/
-        }
-
-        fieldset {
-            border: 1px solid #ddd;
+            font-size: 1.6vw;
+            background: linear-gradient(to right, #4a90e2, #63b8ff);
+            cursor: pointer;
+            padding: 10px;
             border-radius: 2vw;
-            background: linear-gradient(to right, #e4e5dc, #45bac9db);
-            padding: 1vw;
-            box-shadow: 0 0 0.5vw rgba(0, 0, 0, 0.1);
-            margin-bottom: 2vw;
+
+            margin: 10px;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            height: auto;
+            min-height: 40px;
         }
 
+
+        .boton_bus {
+            border: none;
+            outline: none;
+            height: 4vw;
+            color: #fff;
+            font-size: 1.6vw;
+            background: linear-gradient(to right, #4a90e2, #63b8ff);
+            cursor: pointer;
+            border-radius: 60px;
+            width: 60px;
+            margin-top: 2vw;
+            text-decoration: none;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            height: auto;
+
+
+        }
+
+        .boton_bus:active {
+            background-color: #5bc0f7;
+            scale: 1.5;
+            cursor: pointer;
+
+            transition: background-color 0.8s ease, box-shadow 0.8s ease, color 0.8s ease, font-weight 0.8s ease;
+            /* Animaciones de 0.5 segundos */
+            box-shadow: 0 0 5px rgba(91, 192, 247, 0.8), 0 0 10px red;
+            /* Sombra inicial y sombra roja */
+            font-size: 25px;
+            color: white;
+            /* Cambiar el color del texto */
+            font-weight: bold;
+            /* Cambiar a negritas */
+            font-family: "Copperplate", Fantasy;
+        }
+
+        /* Estilos específicos para el modal personalizado */
+        .custom-modal {
+            display: none;
+            position: fixed;
+            z-index: 9999;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0, 0, 0, 0.7);
+        }
+
+        .custom-modal-content {
+            width: 80%;
+            height: 80%;
+            margin: auto;
+            background: linear-gradient(to right, #e4e5dc, #45bac9db);
+            padding: 20px;
+            border-radius: 20PX;
+        }
+
+        .custom-close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .custom-close:hover,
+        .custom-close:focus {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+        /* Estilos adicionales específicos para el iframe dentro del modal */
+        .custom-iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
 
         .clasebotonVER {
             border: none;
@@ -97,6 +260,7 @@ include("include/conec.php");
             color: #080808;
             font-size: 16px;
             padding: 7px;
+            margin: 5px;
 
         }
 
@@ -105,274 +269,71 @@ include("include/conec.php");
             box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
         }
 
-        .dt-button.dtbotopersonal {
-            border: none;
-            outline: none;
-            background: linear-gradient(to right, #4a90e2, #63b8ff);
-            border-radius: 7px;
-            width: auto;
-            text-decoration: none;
-            height: 40px;
-            color: #fff;
-            font-size: 16px;
-            padding: 7px;
+        .dataTables_wrapper {
+            position: relative;
+            clear: both;
         }
 
-        .dt-button.dtbotopersonal:hover {
-            background: linear-gradient(to right, #63b8ff, #4a90e2);
+        .dataTables_wrapper:after {
+            visibility: hidden;
+            display: block;
+            content: "";
+            clear: both;
+            height: 0;
         }
 
-        fieldset fieldset legend {
-            font-size: 20px;
-            text-transform: uppercase;
-            padding-left: 10%;
-            padding-right: 10%;
-            background-color: transparent;
-        }
-
-        legend {
-            font-weight: bold;
-            font-size: 30px;
-            font-weight: bold;
-            margin-bottom: 1vw;
+        fieldset {
+            border: 1px solid #ddd;
+            border-radius: 2vw;
             background: linear-gradient(to right, #e4e5dc, #45bac9db);
-            border: solid 1px #45bac9db;
-            border-radius: 10px;
-        }
-
-        input[type="search"] {
-            /* Tus estilos personalizados aquí */
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            padding: 5px;
-            background-color: #f2f2f2;
-            color: #333;
-            width: 200px;
-            /* Ancho personalizado */
-        }
-
-        .dataTables_filter input {
-            color: white;
-            background-color: red;
-        }
-
-        .dataTables_wrapper .dataTables_filter input {
-            width: 170px;
-            padding: 10px;
-            font-size: 1vw;
-            color: #444;
+            padding: 1vw;
+            box-shadow: 0 0 0.5vw rgba(0, 0, 0, 0.1);
             margin-bottom: 2vw;
-            border: none;
-            border-bottom: 0.1vw solid #444;
-            outline: none;
-            border-radius: 15px;
-            margin: 10px;
-            background-color: white;
-
-        }
-
-        .dataTables_wrapper .dataTables_length,
-        div.dataTables_wrapper div.dataTables_filter label,
-        div.dataTables_wrapper div.dataTables_info {
-            color: black;
-            font-weight: bold;
-
-        }
-
-        .dataTables_wrapper .dataTables_paginate .paginate_button {
-            box-sizing: border-box;
-            display: inline-block;
-            min-width: 1.5em;
-            /*padding: 0.5em 1em;*/
-            margin-left: 2px;
-            text-align: center;
-            /*text-decoration: none !important;*/
-            cursor: pointer;
-            color: #fff;
-            border: 1px solid transparent;
-
-
-            background: linear-gradient(to right, #4a90e2, #63b8ff);
-            border-radius: 7px;
-            width: auto;
-            text-decoration: none;
-            height: 40px;
-            font-size: 16px;
-            padding: 7px;
-        }
-
-        .dataTables_wrapper .dataTables_paginate .paginate_button:hover {
-            box-sizing: border-box;
-            display: inline-block;
-            min-width: 1.5em;
-
-            margin-left: 2px;
-            text-align: center;
-
-            cursor: pointer;
-            color: #fff;
-            border: 1px solid transparent;
-
-
-            background: linear-gradient(to right, #63b8ff, #4a90e2);
-            border-radius: 7px;
-            width: auto;
-            text-decoration: none;
-            height: 40px;
-            font-size: 16px;
-            padding: 7px;
-            box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+            height: 500px;
         }
     </style>
+    <?php
 
+    include("menu_lateral_header.php");
 
-<?php
-
-  include("menu_lateral_header.php");
-
-  ?>
-
-
-
+    ?>
 </head>
 <?php
 
-  include("menu_lateral.php");
+include("menu_lateral.php");
 
-  ?>
+?>
+
 
 <body>
-    <?php
 
-    $filasmax = 5;
-
-    if (isset($_GET['pag'])) {
-        $pagina = $_GET['pag'];
-    } else {
-        $pagina = 1;
-    }
-
-    if (isset($_POST['btnbuscar'])) {
-        $buscar = $_POST['txtbuscar'];
-
-        $sqlusu = mysqli_query($conn, "SELECT * FROM usuario where id_usuario = '" . $buscar . "'");
-    } else {
-        $sqlusu = mysqli_query($conn, "SELECT * FROM usuario ORDER BY id_usuario ASC LIMIT " . (($pagina - 1) * $filasmax)  . "," . $filasmax);
-    }
-
-    $resultadoMaximo = mysqli_query($conn, "SELECT count(*) as num_usuarios FROM usuario");
-
-    $maxusutabla = mysqli_fetch_assoc($resultadoMaximo)['num_usuarios'];
-
-    ?>
-    <div class="cont">
+    <div class="container">
         <form method="POST">
-        <fieldset>
-        <legend>Lista de usuarios</legend>
-
-            <a  class='claseboton' href="menu-mant.php">Volver al menu</a>
-            <a  class='claseboton' href="menu.php">Inicio</a>
-
-            <?php echo "<a  class='claseboton' href=\"modulo/user/agregar.php?pag=$pagina\">Crear usuario</a>"; ?>
-            <!--input type="submit" value="Buscar" name="btnbuscar"-->
-            <!--input type="text" name="txtbuscar" placeholder="Ingresar DNI de usuario" autocomplete="off" style='width:20%'-->
+            <fieldset>
+                <legend>Usuarios</legend>
+                <?php echo "<a  class='btn btn-primary boton' href=\"modulo/user/agregar.php?pag=$pagina\"><i class='material-icons' style='font-size:21px;color:#f0f0f0;text-shadow:2px 2px 4px #000000;'>person_add</i> Crear usuario</a>"; ?>
         </form>
-        <script>
-            $(document).ready(function() {
-                $('#ejemplo').DataTable({
-                    dom: 'frtip',
-                  /*  buttons: [
-                        'copy', 'csv', 'excel', 'pdf', 'print'
-                    ]*/
-                });
-            });
-        </script>
-        <table id="ejemplo" class="display nowrap" style="width:100%">
-            <thead>
-                <tr>
-                    <th>id usuario</th>
-                    <th>nombre del usuario</th>
-                    <th>contraseña</th>
-                    <th>estado</th>
-                    <th>nombre completo</th>
-                    <th>rol</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <?php
-
-            while ($mostrar = mysqli_fetch_assoc($sqlusu)) {
-
-                echo "<tr>";
-                echo "<td>" . $mostrar['id_usuario'] . "</td>";
-                echo "<td>" . $mostrar['nombre_usuario'] . "</td>";
-                echo "<td>" . $mostrar['Pass1'] . "</td>";
-                echo "<td>" . $mostrar['estado'] . "</td>";
-                echo "<td>" . $mostrar['nombre_completo'] . "</td>";
-                echo "<td>" . $mostrar['rol'] . "</td>";
-                echo "<td style='width:24%'>
-			
-			<a  class='claseboton' href=\"modulo/user/editar.php?id_usuario=$mostrar[id_usuario]&pag=$pagina\">Modificar</a> 
-			</td>";
-            }
-
-            ?>
-        </table>
-
-        <div style='text-align:right'>
-            <br>
-            <?php echo "Total de usuarios: " . $maxusutabla; ?>
+            <iframe id="modal-iframe" src="consulta_usuario2.php" frameborder="0" style="width: 100%; height: 100%;"></iframe>
+        
+        </fieldset>
+        <div style=" margin-top:-20;padding:0; height:0cm;">
+        <a href="menu.php" id="btnatras" class="btn btn-primary boton"
+            style="width: 120px;vertical-align: baseline; font-weight:bold;">
+            <i class="material-icons" style="font-size:21px;color:#f0f0f0;text-shadow:2px 2px 4px #000000;">menu</i>
+            Menú Gnral
+        </a>
+        <a href="index.php" id="btnatras" class="btn btn-primary boton"
+            style="width: 120px;vertical-align: baseline; font-weight:bold;">
+            <i class="material-icons" style="font-size:21px;color:#f0f0f0;text-shadow:2px 2px 4px #000000;">login</i>
+            Login
+        </a>
+        <a href="menu-mant.php" id="btnatras" class="btn btn-primary boton"
+            style="width: 120px;vertical-align: baseline; font-weight:bold;">
+            <i class="material-icons"
+                style="font-size:21px;color:#f0f0f0;text-shadow:2px 2px 4px #000000;">arrow_back</i> Atrás
+        </a>
+    </div>
         </div>
-    </div>
-    <div style='text-align:right'>
-        <br>
-    </div>
-    <div style="text-align:center">
-        <?php
-        if (isset($_GET['pag'])) {
-            if ($_GET['pag'] > 1) {
-        ?>
-                <a href="Agregaruser.php?pag=<?php echo $_GET['pag'] - 1; ?>">Anterior</a>
-            <?php
-            } else {
-            ?>
-                <a href="#" style="pointer-events: none">Anterior</a>
-            <?php
-            }
-            ?>
-
-        <?php
-        } else {
-        ?>
-            <a  class='claseboton' href="#" style="pointer-events: none">Anterior</a>
-            <?php
-        }
-
-        if (isset($_GET['pag'])) {
-            if ((($pagina) * $filasmax) < $maxusutabla) {
-            ?>
-                <a class='claseboton' href="Agregaruser.php?pag=<?php echo $_GET['pag'] + 1; ?>">Siguiente</a>
-            <?php
-            } else {
-            ?>
-                <a  class='claseboton' href="#" style="pointer-events: none">Siguiente</a>
-            <?php
-            }
-            ?>
-        <?php
-        } else {
-        ?>
-            <a  class='claseboton' href="Agregaruser.php?pag=2">Siguiente</a>
-        <?php
-        }
-
-        ?>
-    </div>
-    <a class='claseboton' href="menu-mant.php">Volver al menu</a>
-
-    </fieldset>
-    </form>
-    </div>
 </body>
 
 </html>
