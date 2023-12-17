b7db12a4016ef52c96f7ccaf9ae6ffda7c473c88
Joel Rosario <joelenrosario@gmail.com>
2023-12-17 00:06:16 -0400

    agregar de usuario, estilos cambiados.

---
+++ consulta_usuario2.php
@@ -0,0 +1,395 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "pediatra_sis";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+  die("Error de conexión: " . $conn->connect_error);
+}
+
+// Consulta para obtener los datos de la tabla "usuario"
+$query = "SELECT id_usuario, nombre_usuario, nombre_completo, rol FROM usuario";
+$result = $conn->query($query);
+
+// Función para obtener los datos del usuario por ID
+function obtenerDatosUsuario($idUsuario, $conn)
+{
+  $query = "SELECT nombre_usuario, nombre_completo FROM usuario WHERE id_usuario = '$idUsuario'";
+  $result = $conn->query($query);
+
+  if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $datosUsuario = array('nombre_usuario' => $row['nombre_usuario'], 'nombre_completo' => $row['nombre_completo']);
+    return $datosUsuario;
+  } else {
+    return false;
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- Enlaces a los archivos CSS externos -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
+
+    <!-- Enlaces a los scripts de JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <style>
+    .dataTables_wrapper .dataTables_filter input {
+        border: 1px solid #aaa;
+        border-radius: 3px;
+        padding: 5px;
+        background-color: white;
+        color: inherit;
+        margin-left: 3px;
+    }
+    </style>
+    <style>
+    .dataTables_wrapper .dataTables_filter input {
+        border: 1px solid #aaa;
+        border-radius: 3px;
+        padding: 5px;
+        background-color: white;
+        color: inherit;
+        margin-left: 3px;
+    }
+
+    tr:hover {
+        background-color: #A8A4DE;
+    }
+
+    .resaltado {
+        background-color: #A8A4DE;
+    }
+
+    #tabla_usuarios tbody tr:hover {
+        background-color: #A8A4DE;
+        cursor: pointer;
+    }
+
+    #tabla_usuarios tbody tr:active {
+        background-color: #5bc0f7;
+        cursor: pointer;
+        border: 4px solid red;
+        transition: background-color 0.8s ease, box-shadow 0.8s ease, color 0.5s ease, font-weight 0.8s ease;
+        /* Animaciones de 0.5 segundos */
+        box-shadow: 0 0 5px rgba(91, 192, 247, 0.8), 0 0 10px red;
+        /* Sombra inicial y sombra roja */
+        font-size: 25px;
+        color: white;
+        /* Cambiar el color del texto */
+        font-weight: bold;
+        /* Cambiar a negritas */
+        font-family: "Copperplate", Fantasy;
+    }
+    </style>
+
+
+    <style>
+    .caja {
+        border: 3px solid #ddd;
+        padding: 10px;
+        box-shadow: 0 0 0.5vw rgba(0, 0, 0, 0.1);
+        margin: 10px;
+        border-radius: 5px;
+    }
+
+    .cajalegend {
+        border: 0px solid rgba(102, 153, 144, 0.0);
+        font-weight: bolder;
+        font-size: 16px;
+        color: white;
+        margin: 0;
+        padding: 0;
+        background-color: transparent;
+        border-radius: 2px;
+        margin-top: -20px;
+        text-shadow: 2px 1px 2px #000000;
+
+
+    }
+
+    .container {
+        display: grid;
+        grid-template-columns: 80%;
+        grid-template-rows: repeat(3, 1fr);
+        grid-gap: 6px 10px;
+        margin-left: 10%;
+        margin-right: 10%;
+    }
+
+    label {
+        font-size: 14px;
+        color: #444;
+        margin: 8px;
+        font-weight: bold;
+    }
+
+    button,
+    input,
+    optgroup,
+    select,
+    textarea {
+        margin: 0;
+
+        font-size: 12px;
+        line-height: 14px;
+        margin: 10px;
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+
+    input[type="text"],
+    input[type="date"],
+    select {
+
+        width: 150px;
+        height: 40px;
+        color: #444;
+        margin-bottom: 6%;
+        border: none;
+        border-bottom: 0.1vw solid #444;
+        outline: none;
+        border-radius: 10px;
+
+    }
+
+    button {
+        border: none;
+        outline: none;
+        color: #fff;
+        font-size: 1.6vw;
+        background: linear-gradient(to right, #4a90e2, #63b8ff);
+        cursor: pointer;
+        padding: 10px;
+        border-radius: 2vw;
+
+        margin: 10px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        height: auto;
+        min-height: 40px;
+    }
+
+
+    .boton_bus {
+        border: none;
+        outline: none;
+        height: 4vw;
+        color: #fff;
+        font-size: 1.6vw;
+        background: linear-gradient(to right, #4a90e2, #63b8ff);
+        cursor: pointer;
+        border-radius: 60px;
+        width: 60px;
+        margin-top: 2vw;
+        text-decoration: none;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        height: auto;
+
+
+    }
+
+    .boton_bus:active {
+        background-color: #5bc0f7;
+        scale: 1.5;
+        cursor: pointer;
+
+        transition: background-color 0.8s ease, box-shadow 0.8s ease, color 0.8s ease, font-weight 0.8s ease;
+        /* Animaciones de 0.5 segundos */
+        box-shadow: 0 0 5px rgba(91, 192, 247, 0.8), 0 0 10px red;
+        /* Sombra inicial y sombra roja */
+        font-size: 25px;
+        color: white;
+        /* Cambiar el color del texto */
+        font-weight: bold;
+        /* Cambiar a negritas */
+        font-family: "Copperplate", Fantasy;
+    }
+
+    /* Estilos específicos para el modal personalizado */
+    .custom-modal {
+        display: none;
+        position: fixed;
+        z-index: 9999;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        background-color: rgba(0, 0, 0, 0.7);
+    }
+
+    .custom-modal-content {
+        width: 80%;
+        height: 80%;
+        margin: auto;
+        background: linear-gradient(to right, #e4e5dc, #45bac9db);
+        padding: 20px;
+        border-radius: 20PX;
+    }
+
+    .custom-close {
+        color: #aaa;
+        float: right;
+        font-size: 28px;
+        font-weight: bold;
+    }
+
+    .custom-close:hover,
+    .custom-close:focus {
+        color: #000;
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    /* Estilos adicionales específicos para el iframe dentro del modal */
+    .custom-iframe {
+        width: 100%;
+        height: 100%;
+        border: none;
+    }
+
+    .clasebotonVER {
+        color: #f0f0f0;
+        text-shadow: 2px 2px 4px #000000;
+        font-weight: bold;
+        border: none;
+        outline: none;
+        background: linear-gradient(to right, #05c20e, #84e788);
+        border-radius: 7px;
+        width: auto;
+        text-decoration: none;
+        height: 40px;
+
+        font-size: 16px;
+        padding: 7px;
+        margin: 5px;
+
+    }
+
+    .clasebotonazul:hover {
+        background: linear-gradient(to right, #4a90e2, #63b8ff);
+        box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+    }
+    .clasebotonazul {
+        color: #f0f0f0;
+        text-shadow: 2px 2px 4px #000000;
+        font-weight: bold;
+        border: none;
+        outline: none;
+        background: linear-gradient(to right, #63b8ff,#4a90e2);
+        border-radius: 7px;
+        width: auto;
+        text-decoration: none;
+        height: 40px;
+
+        font-size: 16px;
+        padding: 7px;
+        margin: 5px;
+
+    }
+
+    .clasebotonVER:hover {
+        background: linear-gradient(to right, #84e788, #05c20e);
+        box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+    }
+    </style>
+</head>
+
+<body>
+
+
+    <table id="tabla_usuarios" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>ID Usuario</th>
+                <th>Nombre de Usuario</th>
+                <th>Nombre Completo</th>
+                <th>Rol</th>
+                <th>Acción</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+      // Iterar a través de los resultados de la consulta y generar filas en la tabla
+      if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+          echo "<tr>";
+          echo "<td>" . $row["id_usuario"] . "</td>";
+          echo "<td>" . $row["nombre_usuario"] . "</td>";
+          echo "<td>" . $row["nombre_completo"] . "</td>";
+          echo "<td>" . $row["rol"] . "</td>";
+          echo "<td style='width:24%'> <a class='clasebotonVER' href=\"modulo/user/editar.php?Id_seguro_salud=$row[Id_seguro_salud]&pag=$pagina\"><i class='material-icons' style='font-size:21px;color:#f0f0f0;text-shadow:2px 2px 4px #000000;'>edit</i>Editar</a> </td>";
+          echo "</tr>";
+        }
+      } else {
+        echo "<tr><td colspan='4'>No se encontraron resultados.</td></tr>";
+      }
+      ?>
+        </tbody>
+    </table>
+
+    
+
+
+
+
+
+
+    <script>
+    $(document).ready(function() {
+        $('#tabla_usuarios').DataTable({
+            dom: 'frtip', // Mostrar solo búsqueda y paginación
+            language: {
+                url: '//cdn.datatables.net/plug-ins/1.13.5/i18n/es-ES.json' // Ruta al archivo de traducción
+            }
+        });
+
+        // Asignar un evento de clic a las filas de la tabla
+        $("#tabla_usuarios tbody").on("click", "tr", function() {
+            // Obtener las celdas de la fila clicada
+            var celdas = $(this).find("td");
+
+            // Obtener los datos de las celdas
+            var idUsuario = celdas.eq(0).text();
+            var nombreUsuario = celdas.eq(1).text();
+            var nombreCompleto = celdas.eq(2).text();
+            // Asignar los valores al campo de texto y al label en el otro documento
+            window.parent.document.getElementById("id_usuario").value = idUsuario;
+            window.parent.document.getElementById("nombre_usuario").textContent = nombreUsuario;
+            window.parent.document.getElementById("nombre_completo").textContent = nombreCompleto;
+        });
+
+        // Resto del script similar al código anterior
+    });
+    </script>
+
+</body>
+
+</html>
+
+<?php
+// Cerrar la conexión
+$conn->close();
+?>
--- modulo/user/agregar.php
+++ modulo/user/agregar.php
@@ -12,12 +12,12 @@ $query = "SELECT MAX(id_usuario) AS max_id FROM usuario";
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
-	$row = $result->fetch_assoc();
-	$lastId = $row["max_id"];
-	$newId = $lastId + 1;
+    $row = $result->fetch_assoc();
+    $lastId = $row["max_id"];
+    $newId = $lastId + 1;
 } else {
-	// Si no hay registros en la tabla, asignar el ID inicial
-	$newId = 1;
+    // Si no hay registros en la tabla, asignar el ID inicial
+    $newId = 1;
 }
 
 // Guardar el nuevo ID en una variable PHP
@@ -31,6 +31,15 @@ $userid = $newId;
 
 
     <style>
+        .container {
+            display: grid;
+            grid-template-columns: 80%;
+            grid-template-rows: repeat(3, 1fr);
+            grid-gap: 6px 10px;
+            margin-left: 10%;
+            margin-right: 10%;
+        }
+
         .botones-container2 {
             margin: 2px;
             padding: 2px;
@@ -97,9 +106,21 @@ $userid = $newId;
         }
 
 
-        input {
+        input:read-only {
+            background-color: #63b8ff;
+            border: 2px solid #fff;
+            width: 70px;
+
+        }
+
+        option {
+            width: 75PX;
+        }
+
+        input,
+        select {
             border: none;
-            width: 40%;
+            width: 30%;
             border-radius: 6px;
             padding: 5px;
 
@@ -198,7 +219,8 @@ $userid = $newId;
             border-radius: 10px;
         }
 
-        input[type="search"] {
+        input[type="search"],
+        select {
             /* Tus estilos personalizados aquí */
             border: 1px solid #ccc;
             border-radius: 4px;
@@ -281,102 +303,231 @@ $userid = $newId;
             padding: 7px;
             box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
         }
+
+        .caja {
+            border: 3px solid #ddd;
+            padding: 10px;
+            box-shadow: 0 0 0.5vw rgba(0, 0, 0, 0.1);
+            margin: 10px;
+            border-radius: 5px;
+        }
+
+        .cajalegend {
+            border: 0px solid rgba(102, 153, 144, 0.0);
+            font-weight: bolder;
+            font-size: 16px;
+            color: white;
+            margin: 0;
+            padding: 0;
+            background-color: transparent;
+            border-radius: 2px;
+            margin-top: -20px;
+            text-shadow: 2px 1px 2px #000000;
+
+
+        }
+
+        .boton {
+            border: none;
+            outline: none;
+            height: 4vw;
+            color: #fff;
+            font-size: 14px;
+            background: linear-gradient(to right, #4a90e2, #63b8ff);
+            cursor: pointer;
+            border-radius: 2vw;
+            width: 110px;
+            margin-top: 2vw;
+            text-decoration: none;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            height: auto;
+            min-height: 40px;
+        }
     </style>
-     <script type="text/javascript">
-		// Obtener el campo de entrada y el nuevo ID
-		var txtId = document.getElementById("txtid");
-		var newId = <?php echo $userid; ?>;
-
-		// Asignar el nuevo ID al campo de entrada
-		txtId.value = newId;
-
-		// Cambiar el fondo a gris claro
-		txtId.style.backgroundColor = "#f0f0f0";
-
-		
 
 
-		function placeCursorAtEnd() {
-			if (this.setSelectionRange) {
-				// Double the length because Opera is inconsistent about 
-				// whether a carriage return is one character or two.
-				var len = this.value.length * 2;
-				this.setSelectionRange(len, len);
-			} else {
-				// This might work for browsers without setSelectionRange support.
-				this.value = this.value;
-			}
+    <style>
+        /* Agrega algún estilo si lo deseas */
 
-			if (this.nodeName === "TEXTAREA") {
-				// This will scroll a textarea to the bottom if needed
-				this.scrollTop = 999999;
-			}
-		};
+        input[type="password"] {
+        padding-right: 30px; /* Ajusta el espacio para el ojo */
+        transition: background-color 0.3s; /* Agrega una transición suave para el cambio de color */
+        }
 
-		window.onload = function() {
-			var input = document.getElementById("txtnom");
+        .eye-icon {
 
-			if (obj.addEventListener) {
-				obj.addEventListener("focus", placeCursorAtEnd, false);
-			} else if (obj.attachEvent) {
-				obj.attachEvent('onfocus', placeCursorAtEnd);
-			}
+            right: 5px;
+            top: 50%;
+            transform: translateY(-50%);
+            cursor: pointer;
+        }
 
-			input.focus();
-		}
-	</script>
+        input[type="password"].visible {
+            background-color: #87CEEB;
+            /* Cambia a un color azul cielo cuando es visible */
+        }
+    </style>
+
+    <script type="text/javascript">
+        // Obtener el campo de entrada y el nuevo ID
+        var txtId = document.getElementById("txtid");
+        var newId = <?php echo $userid; ?>;
+
+        // Asignar el nuevo ID al campo de entrada
+        txtId.value = newId;
+
+        // Cambiar el fondo a gris claro
+        txtId.style.backgroundColor = "#f0f0f0";
+
+
+
+
+        function placeCursorAtEnd() {
+            if (this.setSelectionRange) {
+                // Double the length because Opera is inconsistent about 
+                // whether a carriage return is one character or two.
+                var len = this.value.length * 2;
+                this.setSelectionRange(len, len);
+            } else {
+                // This might work for browsers without setSelectionRange support.
+                this.value = this.value;
+            }
+
+            if (this.nodeName === "TEXTAREA") {
+                // This will scroll a textarea to the bottom if needed
+                this.scrollTop = 999999;
+            }
+        };
+
+        window.onload = function() {
+            var input = document.getElementById("txtnom");
+
+            if (obj.addEventListener) {
+                obj.addEventListener("focus", placeCursorAtEnd, false);
+            } else if (obj.attachEvent) {
+                obj.attachEvent('onfocus', placeCursorAtEnd);
+            }
+
+            input.focus();
+        }
+    </script>
+    <?php
+
+    include("../../menu_lateral_header.php");
+
+    ?>
 </head>
+<?php
+
+include("../../menu_lateral.php");
+
+?>
 
 <body>
-    <fieldset>
-        <legend>Agregar Usuario</legend>
-        <form class="form-container" method="POST">
-            <fieldset>
-                <legend> Usuario</legend>
-                <div><label>ID<label>
-                            <input type="text" name="txtid" autocomplete="off" value=<?php echo $userid; ?> require readonly></div>
-                <br>
-                <div><label>Nombre de usuario<label>
-                            <input type="text" autofocus name="txtnom" autocomplete="off" require></div>
-                <br>
-                <div><label>Contraseña</label>
-                    <input type="text" name="txtpass1" autocomplete="off" require>
+    <div class="container">
+        <fieldset style=" height: 1000px;">
+            <legend>Agregar Nuevo Usuario</legend>
+            <form class="form-container2" method="POST">
+                <fieldset class="caja">
+                    <legend class="cajalegend"> ══ DATOS DEL Usuario ══ </legend>
+                    <div><label>ID</label>
+                        <input type="text" name="txtid" autocomplete="off" value=<?php echo $userid; ?> require readonly>
+                    </div>
+                    <br>
+                    <div><label>Nombre de usuario</label>
+                        <input type="text" autofocus name="txtnom" autocomplete="off" require>
+                    </div>
+                    <br>
+
+                    <div>
+                        <label for="password">Contraseña</label>
+                        <input id="password" type="password" name="txtpass1" autocomplete="off" required>
+                        <label style=" font-size:11px; font-weight:bold;margin-left:210px;">Ver</label>
+                        <span style="margin-left:20px;" class="eye-icon" onclick="togglePasswordVisibility('password')">👁️</span>
+                    </div>
+                    <div>
+                        <label for="confirmPassword">Confirmar Pass</label>
+                        <input id="confirmPassword" type="password" name="txtconfi" autocomplete="off" required>
+                        <label style=" font-size:11px; font-weight:bold;margin-left:210px;">Ver</label>
+                        <span style="margin-left:20px;" class="eye-icon" onclick="togglePasswordVisibility('confirmPassword')">👁️</span>
+                    </div>
+
+
+                    <script>
+                        function togglePasswordVisibility(inputId) {
+                            var passwordInput = document.getElementById(inputId);
+                            var eyeIcon = document.querySelector('.eye-icon');
+
+                            if (passwordInput.type === 'password') {
+                                passwordInput.type = 'text';
+                                passwordInput.style.backgroundColor = 'skyblue';
+                                eyeIcon.textContent = '👁️';
+                                passwordInput.classList.add('visible'); // Agrega la clase para cambiar el color de fondo
+                            } else {
+                                passwordInput.type = 'password';
+                                eyeIcon.textContent = '👁️';
+                                passwordInput.style.backgroundColor = 'white';
+                                passwordInput.classList.remove('visible'); // Remueve la clase para volver al color original
+                            }
+                        }
+                    </script>
+                    <br>
+                    <div><label>Estado</label>
+                        <select id="estado" name="txtest" style=" width: 110px; " autocomplete="off" require>
+                            <option selected value="Activo">Activo</option>
+                            <option value="Inactivo">Inactivo</option>
+                        </select>
+                        <!-- <input type="text" name="txtest" autocomplete="off" require> -->
+                    </div>
+                    <br>
+                    <div><label>Nombre/s</label>
+                        <input type="text" name="txtnomcom" autocomplete="off" require>
+                    </div>
+                    <br>
+                    <div><label>Rol</label>
+                        <select id="rol" name="txtrol" style=" width: 110px; " autocomplete="off" require>
+                            <option selected value="Administrador">Administrador</option>
+                            <option value="Secretaria">Secretaría</option>
+                            <option value="Doctor">Doctor</option>
+                        </select>
+                        <!-- <input type="text" name="txtrol" autocomplete="off" require> -->
+                    </div>
+                    <br>
+                </fieldset>
+                <div class="botones-container">
+                    <button class="btn btn-primary boton" type="submit" name="btnregistrar" value="Registrar" onClick="javascript: return confirm('¿Deseas registrar a este *NUEVO* usuario');"> <i class="material-icons" style="font-size:21px;color:#12f333;text-shadow:2px 2px 4px #000000;">add</i> Registrar</button>
+
+                    <!-- <input type="submit" name="btnregistrar" value="Registrar" onClick="javascript: return confirm('¿Deseas registrar a este usuario');"> -->
+
+                    <a href="../../mant-Agregaruser.php" id="btncancel" class="btn btn-primary boton" style="width: 120px;vertical-align: baseline; font-weight:bold;">
+                        <i class="material-icons" style="font-size:21px;color:#f0f0f0;text-shadow:2px 2px 4px #000000;">close</i> Cancelar
+                    </a>
                 </div>
-                <br>
-                <div><label>Confirmar Pass</label>
-                    <input type="text" name="txtconfi" autocomplete="off" require>
-                </div>
-                <br>
-                <div><label>Estado</label>
-                    <input type="text" name="txtest" autocomplete="off" require>
-                </div>
-                <br>
-                <div><label>Nombre/s</label>
-                    <input type="text" name="txtnomcom" autocomplete="off" require>
-                </div>
-                <br>
-                <div><label>Rol</label>
-                    <input type="text" name="txtrol" autocomplete="off" require>
-                </div>
-                <br>
-            </fieldset>
-            <div class='botones-container'>
-                <?php echo "<a href=\"../../mant-agregaruser.php?pag=$pagina\">Cancelar</a>"; ?>
-                <input type="submit" name="btnregistrar" value="Registrar" onClick="javascript: return confirm('¿Deseas registrar a este usuario');">
-            </div>
-        </form>
-        <div class="modal-content" style="width: 100%; height: 80%;">
-            <span class="close">&times;</span>
+            </form>
+
+
             <iframe id="modal-iframe" src="../../consulta_usuario.php" frameborder="0" style="width: 100%; height: 100%;"></iframe>
-        </div>
 
+        </fieldset>
         <div class="botones-container2">
-            <a href="../../mant-Agregaruser.php" class="claseboton">← Atrás</a>
-            <a href="../../index.php" class="claseboton">Login</a>
-            <a href="../../menu.php" class="claseboton">Menú Principal</a>
+            <a href="../../menu.php" id="btnatras" class="btn btn-primary boton" style="width: 120px;vertical-align: baseline; font-weight:bold;">
+                <i class="material-icons" style="font-size:21px;color:#f0f0f0;text-shadow:2px 2px 4px #000000;">menu</i>
+                Menú Gnral
+            </a>
+            <a href="../../index.php" id="btnatras" class="btn btn-primary boton" style="width: 120px;vertical-align: baseline; font-weight:bold;">
+                <i class="material-icons" style="font-size:21px;color:#f0f0f0;text-shadow:2px 2px 4px #000000;">login</i>
+                Login
+            </a>
+            <a href="../../mant-Agregaruser.php" id="btnatras" class="btn btn-primary boton" style="width: 120px;vertical-align: baseline; font-weight:bold;">
+                <i class="material-icons" style="font-size:21px;color:#f0f0f0;text-shadow:2px 2px 4px #000000;">arrow_back</i> Atrás
+            </a>
+
 
         </div>
-    </fieldset>
+
+
 </body>
 
 </html>
