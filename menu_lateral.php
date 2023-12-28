8f065ee738627a2c5cca8acfd816f3a08d0f32b6
Joel Rosario <joelenrosario@gmail.com>
2023-12-28 10:47:11 -0400

    actulizacion menu lateral

--- menu_lateral.php
+++ menu_lateral.php
@@ -1,5 +1,6 @@
 <!-- ▓▓▓▓▓▓▓▓▓▓╬══  MENU LATERAL CÓDIGO ══╬▓▓▓▓▓▓▓▓▓▓ -->
-<a class="btn btn-primary btn-offcanvas boton_bus" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample" >
+<a class="btn btn-primary btn-offcanvas boton_bus" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
+    aria-controls="offcanvasExample">
     <i class="material-icons">menu</i>
 </a>
 
@@ -19,36 +20,96 @@
 
                 <!-- Elementos  acordeón 1 -->
                 <div class="list-group-item list-group-item-action">
-                    <a href="#" data-bs-toggle="collapse" data-bs-target="#submenu_Mantenimientos" data-bs-parent="#submenu-container" style="text-decoration:none" class="list-group-item list-group-item-action">
+                    <a href="#" data-bs-toggle="collapse" data-bs-target="#submenu_Mantenimientos"
+                        data-bs-parent="#submenu-container" style="text-decoration:none"
+                        class="list-group-item list-group-item-action">
                         <i class="material-icons">tabs</i>Mantenimientos</a>
                     <div class="collapse" id="submenu_Mantenimientos" data-bs-parent="#submenu-container">
-                    <a href="mant-Agregaruser.php" class="list-group-item list-group-item-action"> <i class="material-icons">account_circle</i> Usuarios</a>
-                        <a href="menu-mant.php" class="list-group-item list-group-item-action" style="text-align: right;"><i class="material-icons">menu</i> Menu Mant</a>
+                        <a href="mant-Agregaruser.php" class="list-group-item list-group-item-action"> <i
+                                class="material-icons">account_circle</i> Usuarios</a>
+                        <a href="menu-mant.php" class="list-group-item list-group-item-action"
+                            style="text-align: right;"><i class="material-icons">menu</i> Menu Mant</a>
                         <!-- Elementos  acordeón 1.2 -->
                         <div class="list-group-item list-group-item-action">
-                            <a href="menu-pacientes.php" data-bs-toggle="collapse" data-bs-target="#submenu_paciente" data-bs-parent="#submenu-container" style="text-decoration:none" class="list-group-item list-group-item-action"> <i class="material-icons">personal_injury</i> Pacientes</a>
+                            <a href="menu-pacientes.php" data-bs-toggle="collapse" data-bs-target="#submenu_paciente"
+                                data-bs-parent="#submenu-container" style="text-decoration:none"
+                                class="list-group-item list-group-item-action"> <i
+                                    class="material-icons">personal_injury</i> Pacientes</a>
                             <div class="collapse" id="submenu_paciente" data-bs-parent="#submenu-container">
-                                <a href="menu-pacientes.php" class="list-group-item list-group-item-action" style="text-align: right;"><i class="material-icons">menu</i> Menu Pacientes</a>
+                                <a href="menu-pacientes.php" class="list-group-item list-group-item-action"
+                                    style="text-align: right;"><i class="material-icons">menu</i> Menu Pacientes</a>
 
-                                <a href="paciente.php" class="list-group-item list-group-item-action"><i class="material-icons">group</i> Pacientes</a>
-                                <a href="MANT-pacientevacuna.php" class="list-group-item list-group-item-action"><i class="material-icons">vaccines</i> Vacunas-Pacientes</a>
-                                <a href="mant_paciente_historiaClinica.php" class="list-group-item list-group-item-action"><i class="material-icons">healing</i> Padecimientos-Pacientes</a>
+                                <a href="paciente.php" class="list-group-item list-group-item-action"><i
+                                        class="material-icons">group</i> Pacientes</a>
+                                <a href="MANT-pacientevacuna.php" class="list-group-item list-group-item-action"><i
+                                        class="material-icons">vaccines</i> Vacunas-Pacientes</a>
+                                <a href="mant_paciente_historiaClinica.php"
+                                    class="list-group-item list-group-item-action"><i class="material-icons">healing</i>
+                                    Padecimientos-Pacientes</a>
 
                             </div>
 
+
                         </div>
-                        <a href="mant_seguro.php" class="list-group-item list-group-item-action"><i class="material-icons">medical_information</i> Seguros</a>
-                        
-                        <a href="mant-centromedico.php" class="list-group-item list-group-item-action"><span class="material-symbols-outlined">home_health</span> Centro Médico</a>
-                        <a href="mant_medicamentos.php" class="list-group-item list-group-item-action"> <i class="material-icons">local_pharmacy</i> Medicamentos</a>
-                        <a href="mant_vacunas.php" class="list-group-item list-group-item-action"> <span class="material-symbols-outlined">syringe</span> Vacunas</a>
-                        <a href="mant_padecimientos_comunes.php" class="list-group-item list-group-item-action"> <span class="material-symbols-outlined">sick</span> Padecimientos</a>
+                        <div class="list-group-item list-group-item-action">
+                            <a href="menu-pacientes.php" data-bs-toggle="collapse" data-bs-target="#submenu_paciente"
+                                data-bs-parent="#submenu-container" style="text-decoration:none"
+                                class="list-group-item list-group-item-action"> <span class="material-symbols-outlined">
+                                    stethoscope
+                                </span> Médicos</a>
+                            <div class="collapse" id="submenu_paciente" data-bs-parent="#submenu-container">
+                                <a href="menu-medico.php" class="list-group-item list-group-item-action"
+                                    style="text-align: right;"><i class="material-icons">menu</i> Menu Médico</a>
+
+                                <a href="mant_medico.php" class="list-group-item list-group-item-action"><span
+                                        class="material-symbols-outlined">
+                                        stethoscope_check
+                                    </span> Medico</a>
+
+                                <a href="mant_trabajosmedicos.php" class="list-group-item list-group-item-action"><span
+                                        class="material-symbols-outlined">
+                                        clinical_notes
+                                    </span>Trabajos Médicos</a>
+
+                                <a href="mant_especialidad.php" class="list-group-item list-group-item-action"><span
+                                        class="material-symbols-outlined">
+                                        groups
+                                    </span>
+                                    Especialidad</a>
+
+                                <a href="" class="list-group-item list-group-item-action"><span
+                                        class="material-symbols-outlined">
+                                        calendar_month
+                                    </span>
+                                    Horario-Médico</a>
+                            </div>
+                        </div>
+
+                        <a href="mant_seguro.php" class="list-group-item list-group-item-action"><i
+                                class="material-icons">medical_information</i> Seguros</a>
+
+                        <a href="mant-centromedico.php" class="list-group-item list-group-item-action"><span
+                                class="material-symbols-outlined">home_health</span> Centro Médico</a>
+
+                        <a href="mant_medicamentos.php" class="list-group-item list-group-item-action"> <i
+                                class="material-icons">local_pharmacy</i> Medicamentos</a>
+
+                        <a href="mant_vacunas.php" class="list-group-item list-group-item-action">
+                            <span class="material-symbols-outlined">syringe</span> Vacunas</a>
+
+                        <a href="mant_padecimientos_comunes.php" class="list-group-item list-group-item-action">
+                            <span class="material-symbols-outlined">sick</span> Padecimientos</a>
+
+                        <a href="mant_laboratorio.php" class="list-group-item list-group-item-action">
+                            <span class="material-symbols-outlined">biotech</span> Laboratorios</a>
                     </div>
 
                 </div>
                 <!-- Elementos  acordeón 2 -->
                 <div class="list-group-item list-group-item-action">
-                    <a href="#" data-bs-toggle="collapse" data-bs-target="#submenu_procesos" data-bs-parent="#submenu-container" style="text-decoration:none" class="list-group-item list-group-item-action">Procesos</a>
+                    <a href="#" data-bs-toggle="collapse" data-bs-target="#submenu_procesos"
+                        data-bs-parent="#submenu-container" style="text-decoration:none"
+                        class="list-group-item list-group-item-action">Procesos</a>
                     <div class="collapse" id="submenu_procesos" data-bs-parent="#submenu-container">
                         <a href="#" class="list-group-item list-group-item-action">Subitem 1</a>
                         <a href="#" class="list-group-item list-group-item-action">Subitem 2</a>
@@ -57,7 +118,10 @@
                 </div>
                 <!-- Elementos  acordeón 3 -->
                 <div class="list-group-item list-group-item-action">
-                    <a href="#" data-bs-toggle="collapse" data-bs-target="#submenu_procesos" data-bs-parent="#submenu-container" style="text-decoration:none" class="list-group-item list-group-item-action"> <i class="material-icons">analytics</i> Consultas y Reportes</a>
+                    <a href="#" data-bs-toggle="collapse" data-bs-target="#submenu_procesos"
+                        data-bs-parent="#submenu-container" style="text-decoration:none"
+                        class="list-group-item list-group-item-action"> <i class="material-icons">analytics</i>
+                        Consultas y Reportes</a>
                     <div class="collapse" id="submenu_procesos" data-bs-parent="#submenu-container">
                         <a href="#" class="list-group-item list-group-item-action">Subitem 1</a>
                         <a href="#" class="list-group-item list-group-item-action">Subitem 2</a>
@@ -85,7 +149,7 @@
 </div>
 
 <script>
-    document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function () {
         function getCurrentPage() {
             var pathArray = window.location.pathname.split('/');
             var currentPage = pathArray.pop();
@@ -111,7 +175,7 @@
             var currentPage = getCurrentPage();
             var listGroupItems = document.querySelectorAll('.list-group-item-action');
 
-            listGroupItems.forEach(function(item) {
+            listGroupItems.forEach(function (item) {
                 var href = item.getAttribute('href');
                 if (href && href.endsWith(currentPage)) {
                     item.focus();
