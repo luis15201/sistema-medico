648cbe36855cebe7784db7da7819eac05c0a8a45
Joel Rosario <joelenrosario@gmail.com>
2024-01-21 00:24:12 -0400

    pruebas con el backup

---
+++ basededatos/pediatra_sis_20240121_035135.sql
@@ -0,0 +1,2 @@
+mysqldump: [Warning] Using a password on the command line interface can be insecure.
+mysqldump: Got error: 1045: Access denied for user 'Admin_pediatra'@'localhost' (using password: YES) when trying to connect
