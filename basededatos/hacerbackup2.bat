c5361c7760b3ea421361ef49ff1a5ae3ab28eac9
Joel Rosario <etanamunt@gmail.com>
2024-01-22 08:21:30 -0400

    pruebas con el backup

---
+++ basededatos/hacerbackup2.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal
+
+REM Establecer las variables de conexión
+set "servername=localhost"
+set "username=Admin_pediatra"
+set "password=Hola@1024" 
+set "database=pediatra_sis"
+
+REM Obtener la fecha actual en formato YYYYMMDD
+for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (
+    set "fecha=%%c%%b%%a"
+)
+
+REM Definir la ruta y el nombre del archivo de respaldo
+set "ruta=C:\wamp64\www\sistema-medico\basededatos"
+set "nombre=pediatra_sis_%fecha%.sql"
+set "archivo=%ruta%\%nombre%"
+
+REM Ejecutar el comando de respaldo
+"C:\wamp64\bin\mysql\mysql8.0.31\bin\mysqldump" -u %username% -p%password% -h %servername% %database% > %archivo%
+
+REM Verificar si el comando se ejecutó correctamente
+if %errorlevel% equ 0 (
+    echo Backup completado correctamente. El archivo se ha guardado como: %nombre%
+) else (
+    echo Ha ocurrido un error durante el proceso de backup.
+)
+
+pause
