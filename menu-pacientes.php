cd89946b9cdd5081df091fc5695da44ff53d41f6
Joel Rosario <etanamunt@gmail.com>
2024-02-03 21:16:29 -0400

    no coge los datos de de padres

--- menu-pacientes.php
+++ menu-pacientes.php
@@ -173,7 +173,7 @@ include("menu_lateral_header.php");
   </div>
 </a>
 
-<a href="">
+<a href="mant_localizadorp.php">
   <div class="card-wrapper">
   <div class="card" style="background: linear-gradient(to right,#e4e5dc ,#62c4f9 ); ">
       <div class="card-title" style="font-family: Anton; color:black;">localizador Padres</div>
