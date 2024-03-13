33f3c769d89076a55bf992522767cc353d81e072
luis15201 <64227145+luis15201@users.noreply.github.com>
2024-03-13 01:45:26 -0400

    creando la ventana del proceso de certificado medico

--- report1.php
+++ report1.php
@@ -1,41 +1,56 @@
 <?php
 require('fpdf186/fpdf.php');
 
-class PDF extends FPDF
-{
-// Cabecera de página
-function Header()
-{
-    // Logo
-    $this->Image('IMAGENES/historia1.png',4,6,27);
-    // Arial bold 15
-    $this->SetFont('Arial','B',15);
-    // Movernos a la derecha
-    $this->Cell(80);
-    // Título
-    $this->Cell(50,20,'Certificado Medico',1,0,'C');
-    // Salto de línea
-    $this->Ln(20);
+
+class PDF extends FPDF {
+    function Header() {
+        // Título
+        $this->SetFont('Arial','B',14);
+        $this->Cell(0,10,'Consultorio Pediatrico',0,1,'C');
+        $this->Ln(10);
+
+        // Fecha y día
+        $this->SetFont('Arial','',12);
+        $this->Cell(0,10,date('d/m/Y'),0,1,'R');
+        $this->Ln(10);
+    }
+
+    function Footer() {
+        // Línea de firma
+        $this->SetY(-80);
+        $this->SetFont('Arial','I',10);
+        $this->Cell(0,10,'Firma: Nombre del Doctor',0,1,'C');
+    }
+
+    function Body($doctor, $exequatur, $paciente) {
+        $this->Cell(0,10,'Certificado Medico',0,1,'C');
+        // Cuerpo del certificado
+        $this->SetFont('Arial','',12);
+         // Reducir el espacio antes del texto
+        $this->Ln(-80); // Reducir el valor de 10 a 5, o cualquier otro valor que desees
+        
+        // Obtener la altura del texto
+        $texto = utf8_decode('Yo, '.$doctor.', con excequatur '.$exequatur.', certifico que el joven '.$paciente.' luego de hacerle los exámenes médicos de lugar, no padece de ninguna enfermedad y esta en muy buena condición física.');
+        $ancho = 180; // Ancho máximo de la celda, en este caso, el ancho de la página
+        $alto = 8; // Altura de la celda
+        $alturaTexto = ceil($this->GetStringWidth($texto) / $ancho) * $alto;
+    
+        // Calcular la posición vertical para centrar el texto
+        $y = ($this->GetPageHeight() - $this->GetY() - $alturaTexto) / 3 + $this->GetY();
+    
+        // Agregar el texto centrado verticalmente
+        $this->SetY($y);
+        $this->MultiCell($ancho, $alto, $texto, 0, 'C');
+    }
 }
 
-// Pie de página
-function Footer()
-{
-    // Posición: a 1,5 cm del final
-    $this->SetY(-15);
-    // Arial italic 8
-    $this->SetFont('Arial','I',8);
-    // Número de página
-    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-}
-}
-
-// Creación del objeto de la clase heredada
+// Crear instancia de PDF
 $pdf = new PDF();
-$pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Times','',12);
-for($i=1;$i<=40;$i++)
-    $pdf->Cell(0,10,utf8_decode('Imprimiendo línea número ').$i,0,1, 'C');
+
+// Llamar al método Body para agregar el contenido
+$pdf->Body("Nombre del Doctor", "Numero de exequatur", "Nombre del Paciente");
+
+// Generar el PDF
 $pdf->Output();
 ?>
